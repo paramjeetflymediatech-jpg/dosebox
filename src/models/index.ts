6194624eb1e88d6d38a050c19c49fa91966647fa
Amandeep Kumar <amandeepkumar.flymediatech@gmail.com>
2026-07-04 09:55:46 +0530

    Fix Sequelize Model mock crashing production API routes

--- src/models/index.ts
+++ src/models/index.ts
@@ -2,14 +2,6 @@ import { Model, DataTypes } from 'sequelize';
 import type { Optional } from 'sequelize';
 import sequelize from '../config/database';
 
-if (Model && typeof Model.init !== 'function') {
-  console.log('[Sequelize Mock] Mocking Model methods for Next.js build');
-  (Model as any).init = function() {};
-  (Model as any).hasMany = function() {};
-  (Model as any).belongsTo = function() {};
-  (Model as any).hasOne = function() {};
-  (Model as any).belongsToMany = function() {};
-}
 
 // ----------------------------------------------------
 // 1. ROLE
