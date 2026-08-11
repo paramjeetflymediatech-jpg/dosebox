481600b6bd72d92845d8a972e7d6afbf4c2ea563
Amandeep Kumar <amandeepkumar.flymediatech@gmail.com>
2026-08-11 13:06:55 +0530

    google login problem in mobile

--- mobile/src/config/api.ts
+++ mobile/src/config/api.ts
@@ -4,5 +4,5 @@ import { Platform } from 'react-native';
  export const API_BASE_URL = "https://dosebox.in/api";
 
 // Development (Uncomment to use local backend)
-// const DEV_URL = Platform.OS === 'android' ? "http://192.168.1.5:3000/api" : "http://localhost:3000/api";
+// const DEV_URL = Platform.OS === 'android' ? "http://192.168.1.6:3000/api" : "http://localhost:3000/api";
 // export const API_BASE_URL = DEV_URL;
